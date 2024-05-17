d0e42ef23b3416ac15ebc59db9bb95583455df26
Davi Morais <davimorais50@gmail.com>
2024-05-17 05:51:14 -0300

    Adicionando funcionalidade a ContaTerminal.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    Scanner sc = new Scanner(System.in); //create scanner object
+    System.out.println("Enter username"); // input username
+    String username = sc.nextLine(); //read input name
+    System.out.println("Enter agency"); // input username
+    Integer agencia = sc.nextInt(); //read input agencia
+    System.out.println("Enter bank number"); // input username
+    Integer numero = sc.nextInt(); //read input agencia
+    System.out.println("Enter balance"); // input username
+    Double saldo = sc.nextDouble(); //read input agencia
+
+    System.out.println("Olá " +username+ ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia+ ", conta " +numero+ " e seu saldo "+saldo+" já está disponível para saque");
+
+        
     }
 }
